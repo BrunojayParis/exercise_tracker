9a55db095eb1ffcfc5f24ba45e0014ec4fb2d6b7
Bruno Paris <brunojparisz@gmail.com>
2021-03-12 12:31:47 -0300

    Connected to database

--- Backend/index.js
+++ Backend/index.js
@@ -1,3 +1,25 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const  
+const cors = require('cors');
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+//Middlewares
+
+app.use(cors());
+app.use(express.json());
+
+//connect to DB
+const uri = process.env.DATABASE_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true },
+    ()=>{console.log("connected")});
+
+//Server Listening
+
+app.listen(5000,()=>{
+    console.log(`connected to server on port: ${port}`);
+});
+
+
