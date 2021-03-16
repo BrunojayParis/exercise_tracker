37ba4f5ea14bd1927affc275a95f21f768fa0db1
Bruno Paris <brunojparisz@gmail.com>
2021-03-15 22:19:39 -0300

    CRUD for exercises.

--- Backend/routes/exercises.js
+++ Backend/routes/exercises.js
@@ -22,7 +22,27 @@ router.post('/add', (req,res)=>{
     newExercise.save()
     .then(()=> res.json('exercise added!'))
     .catch(err => res.status(400).json(err));
+});
 
+router.delete('/:id',(req,res)=>{
+    Exercise.findByIdAndDelete(req.params.id)
+    .then(()=> res.json('Exercise deleted'))
+    .catch(err => res.status(400).json(err));
+});
+
+router.get('/update/:id', (req,res)=>{
+    Exercise.findById(req.params.id)
+    .then(exercise =>{
+        exercise.userName = req.body.userName;
+        exercise.description = req.body.description;
+        exercise.duration = req.body.duration;
+        exercise.date = req.body.date;
+            exercise.save()
+            .then(()=> res.json("exercise uodated!"))
+            .catch(err => res.status(400).json(err));
+
+    })
+    .catch(err => res.status(400).json(err))
+    
 })
-
 module.exports = router;
