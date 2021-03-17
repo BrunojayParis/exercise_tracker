865b98f2871c4479ae5d76addaaf430bb1cb5fae
Bruno Paris <brunojparisz@gmail.com>
2021-03-17 18:34:41 -0300

    added the components

--- extracker/src/components/Navbar.js
+++ extracker/src/components/Navbar.js
@@ -4,6 +4,9 @@ const Navbar =()=>{
     return(
         <div>
             <nav className="bg-gray-800">
+                <div className="max-w-2xl mx-auto px-2 sm:px-6 lg:px-8">
+                    <div className="    "></div>
+                </div>
             </nav>
         </div>
     )
