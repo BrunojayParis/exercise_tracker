dde329f51de04a2edc8c79b7392bc2b6710af35e
Bruno Paris <brunojparisz@gmail.com>
2021-03-17 15:57:02 -0300

    Installed tailwind and started modeling the frontend

---
+++ extracker/src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Navbar =()=>{
+    return(
+        <div>
+            <nav className="bg-gray-800">
+            </nav>
+        </div>
+    )
+}
+
+export default Navbar;
