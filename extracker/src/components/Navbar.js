d12177bc1389454497d9803e27deae11dbf3b3dd
Bruno Paris <brunojparisz@gmail.com>
2021-03-18 11:30:00 -0300

    2.5 hours to make the navbar

--- extracker/src/components/Navbar.js
+++ extracker/src/components/Navbar.js
@@ -3,9 +3,13 @@ import React from 'react';
 const Navbar =()=>{
     return(
         <div>
-            <nav className="bg-gray-800">
-                <div className="max-w-2xl mx-auto px-2 sm:px-6 lg:px-8">
-                    <div className="    "></div>
+            <nav className="bg-gray-300">
+                <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+                    <div className="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
+                        <div className="flex justify-start lg:w-0 lg:flex-1">
+                            <h1 className="text-3xl text-color-white">Navbar</h1>
+                        </div>
+                    </div>
                 </div>
             </nav>
         </div>
