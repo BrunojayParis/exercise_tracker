6161d1466927dd8035e1faa63ca0da40fb2bb921
Bruno Paris <brunojparisz@gmail.com>
2021-04-02 23:34:34 -0300

    Hooks for the exercise page

--- extracker/src/components/CreateExercise.js
+++ extracker/src/components/CreateExercise.js
@@ -17,9 +17,9 @@ const CreateExercise = () => {
   const addDuration = (e) => {
     setDuration(e.target.value)
   };
-  const submitButton = (e)=>{
+  const submitButton = (e) => {
     e.preventDefault();
-    const exercise ={
+    const exercise = {
       username: userName,
       description: description,
       duration: duration
@@ -30,7 +30,7 @@ const CreateExercise = () => {
 
   return (
     <div className="flex flex-col m-5 justify-start">
-        <h2 className="text-5xl py-8">create new exercise</h2>
+      <h2 className="text-5xl py-8">create new exercise</h2>
       <form className="flex flex-col text-xl max-w-lg py-5">
         <label>username</label>
         <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" onChange={addUsername} />
@@ -39,7 +39,7 @@ const CreateExercise = () => {
         <label>Duration</label>
         <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="number" onChange={addDuration} />
       </form>
-      <input className="inline-block px-6 py-2 text-xs font-medium leading-6 text-center text-white uppercase transition bg-black rounded shadow ripple waves-light hover:shadow-lg focus:outline-none hover:bg-gray-400" type="button" value="submit" onClick={submitButton}/>
+      <input className="inline-block px-6 py-2 text-xs font-medium leading-6 text-center text-white uppercase transition bg-black rounded shadow ripple waves-light hover:shadow-lg focus:outline-none hover:bg-gray-400" type="button" value="submit" onClick={submitButton} />
     </div>
   );
 };
